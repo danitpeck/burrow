b805b4f889af34756d92189185884ffcf6481040
Dani Peck <danitpeck@gmail.com>
2026-03-31 23:05:47 -0500

    Add tasks resource

---
+++ api/src/tasks/tasks.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
+
+@Injectable()
+export class TasksService {
+  create() {
+    console.log('Creating a new task');
+    return { status: 'created' };
+  }
+
+  findAll() {
+    return `This action returns all tasks`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} task`;
+  }
+
+  update(id: number, updateTaskDto: UpdateTaskDto) {
+    return `This action updates a #${id} task`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} task`;
+  }
+}
